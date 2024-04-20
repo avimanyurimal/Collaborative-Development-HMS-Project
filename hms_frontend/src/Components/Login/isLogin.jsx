2b29a09e447ad6471fb9d65f825b7b91fa6a44ce
avimanyurimal <rimal.avimanyu@gmail.com>
2024-04-20 14:13:00 +0545

    for Updating login functionality to fetch user first name in navbar here some necesssary modification is done in isLogin.jsx file

--- hms_frontend/src/Components/Login/isLogin.jsx
+++ hms_frontend/src/Components/Login/isLogin.jsx
@@ -4,9 +4,10 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [isLogin, setIsLogin] = useState(false);
+  const [firstName, setFirstName] = useState(""); // Adding state for first name
 
   return (
-    <UserContext.Provider value={{ isLogin, setIsLogin }}>
+    <UserContext.Provider value={{ isLogin, setIsLogin, firstName, setFirstName }}>
       {children}
     </UserContext.Provider>
   );
