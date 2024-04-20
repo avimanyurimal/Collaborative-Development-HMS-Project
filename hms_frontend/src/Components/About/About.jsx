d8a40e544dfc7810509e2a963184c83e2a5d0d1b
samyog777 <samyogkoirala18@gmail.com>
2024-04-20 16:10:36 +0545

    About Us Bug Fixed

--- hms_frontend/src/Components/About/About.jsx
+++ hms_frontend/src/Components/About/About.jsx
@@ -1,190 +1,48 @@
-import React, { useState } from "react";
-import style from "../Signup/Signup.module.css";
-import Select from "react-select";
-import { validEmail } from "../Signup/regex";
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import style from "./About.module.css";
 
-const options = [
-  { value: "Room1", label: "big" },
-  { value: "Room2", label: "normal" },
-  { value: "Room3", label: "small" },
-];
-
-function BookNowForm({ setForm }) {
-  const [firstName, setFirstName] = useState("");
-  const [firstNameError, setFirstNameError] = useState(true);
-
-  const [lastName, setLastName] = useState("");
-  const [lastNameError, setLastNameError] = useState(true);
-
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [PhoneNumberError, setPhoneNumberError] = useState(true);
-
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState(false);
-  const [address, setAddress] = useState("");
-
-  const [optionRoom, setOptionRoom] = useState(null);
-  const [optionError, setOptionError] = useState(true);
-
-  const handelOptionError = () => {
-    setOptionRoom(options);
-    if (optionRoom === "") {
-      setOptionError(true);
-    } else {
-      setOptionError(false);
-    }
-  };
-
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-  }
-
-  // ! For First Name onChange
-  const HandelFirstNameChange = (event) => {
-    const FIRSTNAME = event.target.value;
-    setFirstName(FIRSTNAME);
-    if (FIRSTNAME.length !== 0) {
-      setFirstNameError(false);
-    } else {
-      setFirstNameError(true);
-    }
-  };
-  // ! For Last Name onChange
-  const HandelLastNameChange = (event) => {
-    const LASTNAME = event.target.value;
-    setLastName(LASTNAME);
-    if (LASTNAME.length !== 0) {
-      setLastNameError(false);
-    } else {
-      setLastNameError(true);
-    }
-  };
-
-  // ! For Number change in number field
-  const handleNumberChange = (e) => {
-    const input = e.target.value;
-    if (/^\d*$/.test(input)) {
-      if (input.length <= 10) {
-        // Limit input length to 10 characters
-        setPhoneNumber(input);
-        if (input.length === 10) {
-          setPhoneNumberError(false); // Set error to false if input length is 10
-        } else {
-          setPhoneNumberError(true); // Set error to true if input length is not 10
-        }
-      }
-    }
-  };
-
-  // ! For Address change in number field
-  const HandelAddressChange = (e) => {
-    const input = e.target.value;
-    setAddress(input);
-  };
-
-  const handleEmailChange = (e) => {
-    const newEmail = e.target.value;
-    setEmail(newEmail);
-    setEmailError(!validEmail.test(newEmail));
-  };
-
-  const handelClose = () => {
-    setForm(false);
-    console.log("Closed");
-  };
-  //! JUST CHAGE IT -->
-  const handelSubmit = () => {
-    if (
-      firstNameError ||
-      lastNameError ||
-      emailError ||
-      PhoneNumberError ||
-      optionError
-    ) {
-      console.log("Error");
-    } else {
-      console.log("Booked");
-    }
-  };
-
+function About() {
   return (
-    <div className="w-96 text-white p-5" id={style["BOOKNOWFORM"]}>
-      <div className={style["Form"]}>
-        <div className={style["Form"]}>
-          <div className={style["form-wrapper"]}>
-            <label htmlFor="">First Name</label>
-            <input
-              value={capitalizeFirstLetter(firstName)}
-              type="text"
-              onChange={HandelFirstNameChange}
-              className={style["form-control"]}
-            />
-          </div>
-          {firstNameError && <p className=" text-red-700">Enter First Name</p>}
-          <div className={style["form-wrapper"]}>
-            <label htmlFor="">Last Name</label>
-            <input
-              value={capitalizeFirstLetter(lastName)}
-              type="text"
-              onChange={HandelLastNameChange}
-              className={style["form-control"]}
-            />
-          </div>
-          {lastNameError && <p className="text-red-700">Enter Last Name</p>}
+    <div className={style["MAIN"]}>
+      <NavBar />
+      <div className={style["AboutUs"]}>
+        <div id={style["firstSection"]} className="text-black text-center">
+          <h2>About MoonLight</h2>
+          <span>
+            We're a new hostel, located in the coolest neighborhood of
+            Kathmandu, Thamel. The area is famous for its hipster vibes, and is
+            the hub of backpackers from around the world. Flock is created by
+            traveler souls and a team driven by Hospitality, keeping in mind,
+            the comforts and needs of a backpacking. Having been stayed in
+            different accommodations, in different parts of the world, we have
+            put together this hostel, so you can have the best of both worlds.
+          </span>
         </div>
-        <div className={style["form-wrapper"]}>
-          <label htmlFor="">Phone Number</label>
-          <input
-            value={phoneNumber}
-            type="text"
-            onChange={handleNumberChange}
-            className={style["form-control"]}
-          />
-          {PhoneNumberError ? (
-            <p className=" text-red-700">Enter a Phone Number</p>
-          ) : null}
-        </div>
-        <div className={style["form-wrapper"]}>
-          <label htmlFor="">Address</label>
-          <input
-            value={address}
-            type="text"
-            onChange={HandelAddressChange}
-            className={style["form-control"]}
-          />
-        </div>
-        <div className={style["form-wrapper"]}>
-          <label htmlFor="">Email</label>
-          <input
-            value={email}
-            className={style["form-control"]}
-            type="text"
-            onChange={handleEmailChange}
-          />
-          {emailError && <p className="text-red-700">Email is not valid</p>}
-        </div>
-        <div>
-          <label htmlFor="roomType">Room Type</label>
-          <Select
-            className={style["OPTIONBOOK"]}
-            onChange={handelOptionError}
-            options={options}
-          />
-          {optionError && <p style={{ color: "red" }}>Plese Select</p>}
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              handelSubmit();
-              handelClose();
-            }}
-            className={style["BTN"]}>
-            Book
-          </button>
+        <div className={style["Contac"]}>
+          <ul className="text-black m-9 p-10 text-center">
+            <li>
+              <span>Phone: +977-32642675</span>
+            </li>
+            <li>
+              <span>Mobile:+977-9856324785</span>
+            </li>
+            <li>
+              <span>Email: reservations@moonlight.com</span>
+            </li>
+            <li>
+              <span>For Collaborations, contact</span>
+            </li>
+            <li>
+              <span>sales@moonlight.com</span>
+            </li>
+          </ul>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
 
-export default BookNowForm;
+export default About;
