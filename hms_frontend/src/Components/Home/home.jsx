379e1056540588d7bcb7ce231138cdd8e2cd7506
samyog777 <samyogkoirala18@gmail.com>
2024-04-21 16:17:50 +0545

    Home modified with resident Home

--- hms_frontend/src/Components/Home/home.jsx
+++ hms_frontend/src/Components/Home/home.jsx
@@ -1,18 +1,19 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/NavBar";
 import ImageSlider from "./ImageSlider";
 import { SliderData } from "./SliderData";
 import "./home.css";
-import { UserContext } from "../Login/isLogin";
+// import { UserContext } from "../Login/isLogin";
 import ResidentHome from "../Resident_Home/ResidentHome";
 
 const Home = () => {
-  const { isLogin } = useContext(UserContext);
+  // const { isLogin } = useContext(UserContext);
   return (
     <div className={"HOME"}>
       <NavBar />
-      {isLogin ? <ResidentHome /> : <ImageSlider slides={SliderData} />}
+      {/* {isLogin ? <ResidentHome /> : <ImageSlider slides={SliderData} />} */}
+      <ImageSlider slides={SliderData} />
       <Footer userName={"userName"} />
     </div>
   );
--- hms_frontend/src/Components/Resident_Home/ResidentHome.jsx
+++ hms_frontend/src/Components/Resident_Home/ResidentHome.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import style from "./Resident.module.css";
 import eating from "../../assets/ResidentHome/eating.jpg";
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
 
 function ResidentHome() {
   return (
-    <div className={style["ResidentHome"]}>
-      <div id={style["OtherProperty"]} className="text-black text-3xl font-bold"> Hello From Resident </div>
-      <div className={style["Form"]}>
-        <div>BreakFast</div>
-        <div>Launch</div>
-        <div>Dinner</div>
+    <div className={style["MAIN"]}>
+      <NavBar />
+      <div className={style["ResidentHome"]}>
+        <div
+          id={style["OtherProperty"]}
+          className="text-black text-3xl font-bold">
+          {" "}
+          Hello From Resident{" "}
+        </div>
+        <div className={style["Form"]}>
+          <div>BreakFast</div>
+          <div>Launch</div>
+          <div>Dinner</div>
+        </div>
       </div>
+      <Footer userName={"userName"} />
     </div>
   );
 }
